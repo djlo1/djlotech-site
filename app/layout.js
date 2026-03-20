2cf58ee2044dbbb8eab1056154b658eddbaaa881
Djlo Alo <62613682+djlo1@users.noreply.github.com>
2026-03-20 01:10:15 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: "DJLOTECH SOCIETY",
+  description: "Solutions digitales innovantes au Bénin",
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
